03d68e43730297cc2e0681750f8603918b79cd19
matheus <matheusgongo@ucl.br>
2022-10-31 21:32:12 -0300

    currency calculate

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_restful import Resource, Api
 from flask_cors import CORS
 import urllib.request, json
@@ -14,35 +14,32 @@ def index():
 
     limits = [1,2,3,4,5,6]
     pokemons_req = []
+    
+    preco_atual = 5.28
+    target = request.form
+    print(target)
+    
+    consumo = request.args.get('distance')
+    distance = request.args.get('fuel')
+    print(consumo)
+    print(distance)
+    uf_list = [
+                    "Acre", "Alagoas", "Amapá", "Amazonas", "Bahia", "Ceará", "Distrito Federal", "Espírito Santo", 
+                     "Goiás", "Maranhão", "Mato Grosso", "Mato Grosso do Sul", "Minas Gerais", 
+                     "Pará", "Paraíba", "Paraná", "Pernambuco", "Piauí", "Rio de Janeiro", "Rio Grande do Norte", "Rio Grande do Sul", "Rondônia", "Roraima", "Santa Catarina", 
+                     "São Paulo", "Sergipe", "Tocantins"
+                ]
 
-    for limit in limits:
-        api = f"https://pokeapi.co/api/v2/pokemon/{limit}"
-        request = requests.get(api)
-        pokemon_load = json.loads(request.content)
-        #pokemons_req.append(pokemon_load)
-
-
-    pokemon_json = []
-    #for pokemon in pokemons_req:
-    #    pokemon_body = {
-    #        "name": pokemon["name"],
-    #       "sprite": pokemon["sprites"]["back_default"],
-    #      "height": pokemon["height"],
-    #     "weight": pokemon["weight"]
-    #}
-    #    pokemon_json.append(pokemon_body)
-        
-    print(pokemon_json)
 
     list_titles = [{"thead": "Altura"}]
 
     print(list_titles)
 
-    return render_template("index.html", items = pokemon_json, list_titles = list_titles)
+    return render_template("index.html", uf_list = uf_list, list_titles = list_titles)
 
 
-@app.route("/new", methods=["POST", "GET"])
-def new():
+@app.route("/currency_travel", methods=["POST"])
+def currency_travel():
     return render_template("form.html")
 
 
